f88fb3f0079afb4d3bd6a584c7f6de3a94b285d8
yauhofung <yauho.fung@student.mec.cuny.edu>
2021-04-05 18:32:40 -0400

    Added Push(), IsEmpty(), and Top() to lab06

--- Labs/Lab06/lab06.cpp
+++ Labs/Lab06/lab06.cpp
@@ -1,11 +1,35 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
 /**********************************
 * define function below here
 **********************************/
+void Push(string &stack, char item)
+{
+	if (stack.length() < 100)
+	{
+		stack += item;
+	}
+}
 
+bool IsEmpty(const string &stack)
+{
+	if (stack.length() == 0)
+	{
+		return true;
+	}
+	return false;
+}
 
+char Top(const string &stack)
+{
+	if (stack.length() != 0)
+	{
+		return stack[stack.length() - 1];
+	}
+	throw "Empty Stack";
+}
 /*********************************
 * define function above here
 *********************************/
