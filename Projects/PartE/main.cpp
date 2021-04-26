9b623fbc9d47e4b99d538d7ccca8f55bfafbcb17
yauhofung <yauho.fung@student.mec.cuny.edu>
2021-04-25 21:28:39 -0400

    Updated Processor main.cpp

--- Projects/PartE/main.cpp
+++ Projects/PartE/main.cpp
@@ -11,7 +11,7 @@ int main()
 	Memory memory(&bus, MEMORY_FILENAME);
 	Processor processor(&bus, &log);
 
-	// adds line 1 by line 3 and stores result in AC
+	// adds line 1 by line 3 and stores result in line 8
 	bus.setAddressString("000000000000");
 	bus.setControlString(MEMORY_READ);
 	memory.Process();
@@ -25,6 +25,75 @@ int main()
 
 	bus.setControlString(PROCESSOR_ADD);
 	processor.Process();
+
+	bus.setControlString(PROCESSOR_SAVE);
+	processor.Process();
+
+	bus.setControlString(MEMORY_WRITE);
+	bus.setAddressString("000000000111");
+	memory.Process();
+
+	// divides line 4 by line 2 and stores result in line 9
+	bus.setAddressString("000000000011");
+	bus.setControlString(MEMORY_READ);
+	memory.Process();
+
+	bus.setControlString(PROCESSOR_LOAD);
+	processor.Process();
+
+	bus.setAddressString("000000000001");
+	bus.setControlString(MEMORY_READ);
+	memory.Process();
+
+	bus.setControlString(PROCESSOR_DIV);
+	processor.Process();
+
+	bus.setControlString(PROCESSOR_SAVE);
+	processor.Process();
+
+	bus.setControlString(MEMORY_WRITE);
+	bus.setAddressString("000000001000");
+	memory.Process();
+
+	// performs logical NOT on line 5 and stores result in line 10
+	bus.setAddressString("000000000100");
+	bus.setControlString(MEMORY_READ);
+	memory.Process();
+
+	bus.setControlString(PROCESSOR_LOAD);
+	processor.Process();
+
+	bus.setControlString(PROCESSOR_NOT);
+	processor.Process();
+
+	bus.setControlString(PROCESSOR_SAVE);
+	processor.Process();
+
+	bus.setControlString(MEMORY_WRITE);
+	bus.setAddressString("000000001001");
+	memory.Process();
+
+	// performs logical AND on line 6 with line 7 and stores result in line 11
+	bus.setAddressString("000000000101");
+	bus.setControlString(MEMORY_READ);
+	memory.Process();
+
+	bus.setControlString(PROCESSOR_LOAD);
+	processor.Process();
+
+	bus.setAddressString("000000000110");
+	bus.setControlString(MEMORY_READ);
+	memory.Process();
+
+	bus.setControlString(PROCESSOR_AND);
+	processor.Process();
+
+	bus.setControlString(PROCESSOR_SAVE);
+	processor.Process();
+
+	bus.setControlString(MEMORY_WRITE);
+	bus.setAddressString("000000001010");
+	memory.Process();
 
 	return 0;
 }
